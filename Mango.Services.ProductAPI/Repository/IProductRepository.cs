92bdafe5c8bad30496af80f61cf6dde17a06f900
Prince Uluka <ulukaprince1996@gmail.com>
2023-02-09 02:20:41 +0100

    Product Repository Implementation Part 1

--- Mango.Services.ProductAPI/Repository/IProductRepository.cs
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -5,7 +5,7 @@ namespace Mango.Services.ProductAPI.Repository
     public interface IProductRepository
     {
         Task<IEnumerable<ProductDto>> GetProducts();
-        Task<ProductDto> GetProductById(int id);
+        Task<ProductDto> GetProductById(int productId);
 
         Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
         Task<bool> DeleteProduct(int productId);
