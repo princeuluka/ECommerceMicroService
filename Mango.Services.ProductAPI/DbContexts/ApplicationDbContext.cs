2a82cefeba168eec8e04f74078f6cffad2709ead
Prince Uluka <ulukaprince1996@gmail.com>
2023-02-09 01:58:54 +0100

    Create tables for Product database

--- Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ProductAPI.DbContexts
 {
@@ -8,5 +9,6 @@ namespace Mango.Services.ProductAPI.DbContexts
         {
                 
         }
+        public DbSet<Product> Products { get; set; }
     }
 }
