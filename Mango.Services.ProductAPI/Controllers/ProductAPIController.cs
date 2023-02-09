a55dc158c152dd9b5e4b0adb26d571a82dfdf585
Prince Uluka <ulukaprince1996@gmail.com>
2023-02-09 17:26:44 +0100

    Product API controller HTTPGET

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mango.Services.ProductAPI.Models.Dto;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.ProductAPI.Controllers
 {
-    public class ProductAPIController : Controller
+    [Route("api/products")]
+    public class ProductAPIController : ControllerBase
     {
-        public IActionResult Index()
+        protected ResponseDto _response;
+        private IProductRepository _productRepository;
+        public ProductAPIController(IProductRepository productRepository)
         {
-            return View();
+            _productRepository= productRepository;
+            this._response = new ResponseDto();
+        }
+        [HttpGet]
+        public async Task<object> Get() 
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();   
+                _response.Result = productDtos;
+                
+            }
+            catch (Exception ex)
+            {
+
+              _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString()};
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> GetById(int id)
+        {
+            try
+            {
+                ProductDto productDtos = await _productRepository.GetProductById(id);
+                _response.Result = productDtos;
+
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
     }
+    
 }
