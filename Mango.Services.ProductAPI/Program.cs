c3bb833f7d30756280fdb9b72534945b0efb9323
Prince Uluka <ulukaprince1996@gmail.com>
2023-02-11 20:26:52 +0100

    Bootstrap V5

--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddSingleton<IMapper, Mapper>();
+//builder.Services.AddSingleton<IMapper, Mapper>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
